b73e8cf6d3bf6ca08f1c0cc36dae066df9ca8d05
Darren Tebo <darrentebo83@outlook.com>
2020-07-19 21:49:14 -0400

    wrote checkout tests and began writing shopping cart tests

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,19 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export const useForm = (initialValues) => {
+    const [values, setValues] = useState(initialValues);
+
+    const handleChanges = e => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const clearForm = e => {
+        setValues(initialValues);
+    }
+
+    return [values, handleChanges, clearForm ];
+};
